0b67b3d3b2b1f2150cf4701ea7934ce15ac55904
jiongyi-cao <jiongyi@uchicago.edu>
2019-06-13 19:51:28 +0800

    upload OHIE dataset

---
+++ R/ohieData.R
@@ -0,0 +1,9 @@
+#' Survey respondents dataset from Oregon Health Insurance Experiment (OHIE)
+#'
+#' The original survey data consists 58405 survey participants in total,
+#' including both 23714 respondents and 51184 non-respondents.
+#' To keep congruent with Finkelstein’s analysis
+#' this dataset only consists complete cases, which regards 23714 survey respondents.
+#' @source \url{http://www.nber.org/oregon/4.data.html}
+"ohieData"
+
