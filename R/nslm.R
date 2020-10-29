b23b37a1c2e6e9c2d31e97163194528c4b56f264
jiongyi-cao <jiongyi@uchicago.edu>
2020-10-29 17:36:09 -0500

    upload nslm data

---
+++ R/nslm.R
@@ -0,0 +1,23 @@
+#' National Study of Learning Mindsets data in ACIC 2018 workshop
+#'
+#' Data from The National Study of Learning Mindsets, a randomized controlled trial in
+#' a national probability sample of U.S. public high schools.
+#' This dataset is from the replication files for the paper
+#' "Estimating Treatment Effects with Causal Forests: An Application", by Athey and Wager.
+#'
+#'
+#' @docType data
+#'
+#' @usage data(nslm)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references Athey, S., & Wager, S. (2019). Estimating treatment effects with causal forests: An application.
+#' (\href{https://arxiv.org/pdf/1902.07409.pdf}{arXiv})
+#'
+#' @source \href{http://mindsetscholarsnetwork.org/about-the-network/current-initatives/national-mindset-study/}
+#' @source \href{github.com/grf-labs/grf/experiments/acic18}
+"nslm"
+
